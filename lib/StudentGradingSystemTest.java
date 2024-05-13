c8077a0c9602f265ae23548ce5af7fdf0886e1b2
Rozina-Gebrehiwet <166912787+Rozinagebrehiwet@users.noreply.github.com>
2024-05-13 16:29:08 +0000

    updated

--- lib/StudentGradingSystemTest.java
+++ lib/StudentGradingSystemTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
     public void testAddGrade() {
         GradingSystem system = new GradingSystem();
         system.addStudent("Kebede", 101);
-        system.addGrade(101, 95); // Add grade 90 for student with ID 101
+        system.addGrade(101, 95); // Add grade 95 for student with ID 101
         Assertions.assertEquals(95, system.getAverageGrade(101));
     }
      
