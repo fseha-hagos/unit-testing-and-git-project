a3d8379a94af91866b684e2eb7b8dcbd209bfdeb
fh <fsehah24@gmail.com>
2024-05-14 00:47:27 -0300

    all files must be in src dir and all tests in lib dir

---
+++ src/StudentGrading.java
@@ -0,0 +1,8 @@
+
+public class StudentGrading {
+
+    ///////////////////////////////////////////////////
+    public static void main(String[] args) {
+        System.out.println("Hello Their");
+    }
+}
