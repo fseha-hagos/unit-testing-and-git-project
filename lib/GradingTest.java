39fa39ee46de34576fe215815a844318db56f4df
Rozina-Gebrehiwet <166912787+Rozinagebrehiwet@users.noreply.github.com>
2024-05-16 10:37:29 +0300

    Update GradingTest.java

--- lib/GradingTest.java
+++ lib/GradingTest.java
@@ -1,28 +1,32 @@
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.junit.*;
 
 public class GradingTest {
 
-    Grading grading;
+    @Test
+    public void testAddStudentGrade_FirstYear() {
+        Grading grading = new Grading("1", "fish", "1", "Male");
 
-    @BeforeEach
-    void setUp() {
-        grading = new Grading("ugr/121212", "Kiki", "1999", "Female");
+        assertTrue(grading.AddStudentGrade("0", "1", "4", "3/3/4/4", "SE101/SE102/SE103/SE104", 
+                    "Programming/Networking/COA/Operating"));
     }
 
     @Test
-    void testAddStudentGrade() {
-        boolean result = grading.AddStudentGrade("ugr/121212", "1999", "4", "3/2/4/3", "SE101/SE102/SE103/SE104",
-                "Programming/Networking/COA/Operating");
-        Assertions.assertTrue(result);
+    public void testAddStudentGrade_SeniorYear() {
+        Grading grading = new Grading("1", "nani", "4", "Female");
+
+        assertTrue(grading.AddStudentGrade("5", "4", "3", "3/3/4", "SE401/SE402/SE403", 
+                    "AdvancedProgramming/Security/Database"));
     }
 
     @Test
-    void testAddStudentGradeWithInvalidYear() {
-        boolean result = grading.AddStudentGrade("0", "5", "4", "3/2/4/3", "SE101/SE102/SE103/SE104",
-                "Programming/Networking/COA/Operating");
-        Assertions.assertFalse(result);
+    public void testAddStudentGrade_InvalidInput() {
+        Grading grading = new Grading("1", "abdat", "3", "Male");
+
+        assertFalse(grading.AddStudentGrade("0", "3", "4", "3/3/4/3", "SE101/SE102/SE103/SE104", 
+                    "Programming/Networking/COA/Operating"));
     }
+
+
 }
