75081e7b2ee82edda88f30a2a77ed23b2cce1796
Soltsige <solomontsige2021@gmail.com>
2024-05-16 08:36:44 +0300

    student adding section is added with exception handling

--- src/course.java
+++ src/course.java
@@ -32,3 +32,42 @@ abstract class Course {
     }
 
 }
+//adding student score section 
+public String addStudentScore() {
+
+    double score = 0;
+    boolean right = false;
+    String c_code = super.getCourseCode();
+    String c_name = super.getCourseName();
+    while (!right) {
+        try {
+            System.out.print("Programming (1-4) : ");
+            score = scanner.nextDouble();
+
+            if (score > 0 && score <= 100)
+                right = true;
+            if (score < 50) {
+                score = 1;
+            } else if (score < 65) {
+                score = 2;
+            } else if (score < 80) {
+                score = 3;
+            } else if (score <= 100) {
+                score = 4;
+            }
+        }
+
+                 //exception handling section
+        catch (InputMismatchException e) {
+            System.out.print("please enter between (1-4) : ");
+            scanner.nextLine();
+        }
+    }
+
+    return (c_code + "," + c_name + "," + score);
+}
+
+
+        
+
+    
