92c79dba538fc29e164391e3bb7e53faa1851f8c
abdat-yassin <Abdatyassin1@gmail.com>
2024-05-17 04:08:18 -0700

    add test valid grade

--- lib/StudentTest/StudentTestCalculateCgpa.java
+++ lib/StudentTest/StudentTestCalculateCgpa.java
@@ -23,3 +23,19 @@ public class StudentTest {
         // Assert
         assertEquals(0.0, result, "CGPA should be 0.0 when an 'I' grade is present");
     }
+    
+    @Test
+    public void calculateCGPA_withValidGrades_returnsCorrectCGPA() {
+        // Arrange
+        Course course1 = new Course("MATH101", 3.0, 'A');
+        Course course2 = new Course("PHYS201", 4.0, 'B');
+        student.addCourse(course1);
+        student.addCourse(course2);
+
+        // Act
+        double result = student.calculateCGPA();
+
+        // Assert
+        assertEquals(3.25, result, "CGPA should be calculated correctly for valid grades");
+    }
+    
