cf522ec7d7a6fd736165b9045c37f187e8601be8
abdat-yassin <Abdatyassin1@gmail.com>
2024-05-17 04:13:01 -0700

    add some test

--- lib/StudentTest/StudentTestCalculateCgpa.java
+++ lib/StudentTest/StudentTestCalculateCgpa.java
@@ -49,11 +49,29 @@ public class StudentTest {
         student.addCourse(course2);
         student.addCourse(course3);
 
+
         // Act
         double result = student.calculateCGPA();
 
         // Assert
         assertEquals(3.1666666666666665, result, "CGPA should be calculated correctly for weighted grades");
     }
+    @Test
+public void calculateCGPA_withMultipleCoursesDifferentGrades_returnsWeightedAverage() {
+    // Arrange
+    Student student = new MockStudent("12345", "John Doe", "Sophomore", "Male");
+    Course course1 = new Course("MATH101", 3.0, 'A');
+    Course course2 = new Course("PHYS201", 4.0, 'B');
+    Course course3 = new Course("CHEM101", 2.0, 'C');
+    student.addCourse(course1);
+    student.addCourse(course2);
+    student.addCourse(course3);
+
+    // Act
+    double result = student.calculateCGPA();
+
+    // Assert
+    assertEquals(3.1666666666666665, result, "CGPA should be the weighted average of all courses when courses have different grades");
+}
 }
 
